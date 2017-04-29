016377e46f0244d2d5eed8314025dbdaf342e7ba
Mateusz Pusz <mateusz.pusz@gmail.com>
2017-04-29 20:31:55 +0200

    Project renamed to 'inplace_string' + 'mp' namespace added

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 [![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg?maxAge=3600)](https://raw.githubusercontent.com/mpusz/in_place_string/master/LICENSE)
-[![Travis CI](https://img.shields.io/travis/mpusz/in_place_string/master.svg?label=Travis%20CI)](https://travis-ci.org/mpusz/in_place_string)
-[![AppVeyor](https://img.shields.io/appveyor/ci/mpusz/in-place-string/master.svg?label=AppVeyor)](https://ci.appveyor.com/project/mpusz/in-place-string)
-[![Codecov](https://img.shields.io/codecov/c/github/mpusz/in_place_string/master.svg)](https://codecov.io/github/mpusz/in_place_string?branch=master)
+[![Travis CI](https://img.shields.io/travis/mpusz/inplace_string/master.svg?label=Travis%20CI)](https://travis-ci.org/mpusz/inplace_string)
+[![AppVeyor](https://img.shields.io/appveyor/ci/mpusz/inplace-string/master.svg?label=AppVeyor)](https://ci.appveyor.com/project/mpusz/inplace-string)
+[![Codecov](https://img.shields.io/codecov/c/github/mpusz/inplace_string/master.svg)](https://codecov.io/github/mpusz/inplace_string?branch=master)
 
-# `in_place_string<CharT, MaxSize, Traits>`
+# `mp::inplace_string<CharT, MaxSize, Traits>`
 
-`in_place_string<CharT, MaxSize, Traits>` is a `std::string`-like class template with the difference
+`mp::inplace_string<CharT, MaxSize, Traits>` is a `std::string`-like class template with the difference
 that the text content is always stored in-place inside the class (like in SSO case in `std::string`).
--- src/tests/tests.cpp
+++ src/tests/tests.cpp
@@ -20,17 +20,19 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include "in_place_string.h"
+#include "inplace_string.h"
 #include <gtest/gtest.h>
 
 // explicit instantiation needed to make code coverage metrics work correctly
-template class basic_in_place_string<char, 16, std::char_traits<char>>;
+template class mp::basic_inplace_string<char, 16, std::char_traits<char>>;
+
+using namespace mp;
 
 TEST(inPlaceString, CompileTime) {}
 
 TEST(inPlaceString, DefaultConstructor)
 {
-  in_place_string<16> str;
+  inplace_string<16> str;
   EXPECT_TRUE(str.empty());
   EXPECT_EQ(0u, str.size());
   EXPECT_STREQ("", str.c_str());
@@ -41,7 +43,7 @@ TEST(inPlaceString, DefaultConstructor)
 
 TEST(inPlaceString, UserConstructorPtrSize1)
 {
-  in_place_string<16> str{"", 0};
+  inplace_string<16> str{"", 0};
   EXPECT_TRUE(str.empty());
   EXPECT_EQ(0u, str.size());
   EXPECT_STREQ("", str.c_str());
@@ -52,7 +54,7 @@ TEST(inPlaceString, UserConstructorPtrSize1)
 
 TEST(inPlaceString, UserConstructorPtrSize2)
 {
-  in_place_string<16> str{"test", 4};
+  inplace_string<16> str{"test", 4};
   EXPECT_FALSE(str.empty());
   EXPECT_EQ(4u, str.size());
   EXPECT_STREQ("test", str.c_str());
@@ -64,7 +66,7 @@ TEST(inPlaceString, UserConstructorPtrSize2)
 
 TEST(inPlaceString, UserConstructorPtrSize3)
 {
-  in_place_string<4> str{"test", 4};
+  inplace_string<4> str{"test", 4};
   EXPECT_FALSE(str.empty());
   EXPECT_EQ(4u, str.size());
   EXPECT_STREQ("test", str.c_str());
@@ -77,7 +79,7 @@ TEST(inPlaceString, UserConstructorPtrSize3)
 TEST(inPlaceString, UserConstructorPtrSize4)
 {
   const char s[] = {'t', '\0', 'e', '\0', 's', '\0', 't', '\0'};
-  in_place_string<16> str{s, sizeof(s)};
+  inplace_string<16> str{s, sizeof(s)};
   EXPECT_FALSE(str.empty());
   EXPECT_EQ(8u, str.size());
   EXPECT_STREQ("t", str.c_str());
@@ -89,7 +91,7 @@ TEST(inPlaceString, UserConstructorPtrSize4)
 
 TEST(inPlaceString, UserConstructorPtr1)
 {
-  in_place_string<16> str{""};
+  inplace_string<16> str{""};
   EXPECT_TRUE(str.empty());
   EXPECT_EQ(0u, str.size());
   EXPECT_STREQ("", str.c_str());
@@ -100,7 +102,7 @@ TEST(inPlaceString, UserConstructorPtr1)
 
 TEST(inPlaceString, UserConstructorPtr2)
 {
-  in_place_string<16> str{"test"};
+  inplace_string<16> str{"test"};
   EXPECT_FALSE(str.empty());
   EXPECT_EQ(4u, str.size());
   EXPECT_STREQ("test", str.c_str());
@@ -112,7 +114,7 @@ TEST(inPlaceString, UserConstructorPtr2)
 
 TEST(inPlaceString, UserConstructorPtr3)
 {
-  in_place_string<4> str{"test"};
+  inplace_string<4> str{"test"};
   EXPECT_FALSE(str.empty());
   EXPECT_EQ(4u, str.size());
   EXPECT_STREQ("test", str.c_str());
@@ -125,7 +127,7 @@ TEST(inPlaceString, UserConstructorPtr3)
 TEST(inPlaceString, UserConstructorPtr4)
 {
   const char s[] = {'t', '\0', 'e', '\0', 's', '\0', 't', '\0'};
-  in_place_string<16> str{s};
+  inplace_string<16> str{s};
   EXPECT_FALSE(str.empty());
   EXPECT_EQ(1u, str.size());
   EXPECT_STREQ("t", str.c_str());
